15e9f3e90b939bd4626dc67e260230ed8f4946b8
lucaspaiva <lukspaiva@hotmail.com>
2016-08-11 23:51:06 -0300

    informacoes

--- pbl2/Cliente/src/util/InformacoesCliente.java
+++ pbl2/Cliente/src/util/InformacoesCliente.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package servidor;
+package util;
 
 import java.util.ArrayList;
 
---
+++ pbl2/Servidor/src/util/InformacoesCliente.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author cleyb
+ */
+public class InformacoesCliente {
+    private ArrayList<String> nomeArquivos;
+    private String ip;
+    private int porta;
+
+    public InformacoesCliente(ArrayList<String> nomeArquivos, String ip, int porta) {
+        this.nomeArquivos = nomeArquivos;
+        this.ip = ip;
+        this.porta=porta;
+    }
+
+    public ArrayList<String> getNomeArquivos() {
+        return nomeArquivos;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public int getPorta() {
+        return porta;
+    }
+    
+    
+    
+}
