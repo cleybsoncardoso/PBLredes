1a19553a140e6f132a84269d6ec14ab93b0c32fc
Cleybson Cardoso <cleybsonc@gmail.com>
2016-08-31 00:32:47 -0300

    implementando trajetos

--- PBL3/pbl3/src/model/Auxiliar.java
+++ PBL3/pbl3/src/model/Auxiliar.java
@@ -52,53 +52,53 @@ public class Auxiliar {
 
         }
     }
-    
-    public ArrayList<Quadrante> calcularTrajeto(String origem, String destino){
+
+    public ArrayList<Quadrante> calcularTrajeto(String origem, String destino) {
         ArrayList<Quadrante> trajeto = new ArrayList<Quadrante>();
-        if(origem.equals("A")){
+        if (origem.equals("A")) {
             trajeto.add(new Quadrante("d", 0, 0, 1));
-            if(destino.equals("B")){
+            if (destino.equals("B")) {
                 return trajeto;
-            }else if(destino.equals("C")){
+            } else if (destino.equals("C")) {
                 trajeto.add(new Quadrante("b", 0, 0, 2));
                 return trajeto;
-            }else{
+            } else {
                 trajeto.add(new Quadrante("b", 0, 0, 2));
                 trajeto.add(new Quadrante("a", 0, 0, 3));
                 return trajeto;
             }
-        }else if(origem.equals("B")){
+        } else if (origem.equals("B")) {
             trajeto.add(new Quadrante("b", 0, 0, 1));
-            if(destino.equals("C")){
+            if (destino.equals("C")) {
                 return trajeto;
-            }else if(destino.equals("D")){
+            } else if (destino.equals("D")) {
                 trajeto.add(new Quadrante("a", 0, 0, 2));
                 return trajeto;
-            }else{
+            } else {
                 trajeto.add(new Quadrante("a", 0, 0, 2));
                 trajeto.add(new Quadrante("c", 0, 0, 3));
                 return trajeto;
             }
-        }else if(origem.equals("C")){
+        } else if (origem.equals("C")) {
             trajeto.add(new Quadrante("a", 0, 0, 1));
-            if(destino.equals("D")){
+            if (destino.equals("D")) {
                 return trajeto;
-            }else if(destino.equals("A")){
+            } else if (destino.equals("A")) {
                 trajeto.add(new Quadrante("c", 0, 0, 2));
                 return trajeto;
-            }else{
+            } else {
                 trajeto.add(new Quadrante("c", 0, 0, 2));
                 trajeto.add(new Quadrante("d", 0, 0, 3));
                 return trajeto;
             }
-        }else {
+        } else {
             trajeto.add(new Quadrante("c", 0, 0, 1));
-            if(destino.equals("A")){
+            if (destino.equals("A")) {
                 return trajeto;
-            }else if(destino.equals("B")){
+            } else if (destino.equals("B")) {
                 trajeto.add(new Quadrante("d", 0, 0, 2));
                 return trajeto;
-            }else{
+            } else {
                 trajeto.add(new Quadrante("d", 0, 0, 2));
                 trajeto.add(new Quadrante("b", 0, 0, 3));
                 return trajeto;
