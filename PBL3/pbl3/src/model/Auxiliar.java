fd17bc6adf1656867bfff4351e0e3281b9740a18
Cleybson Cardoso <cleybsonc@gmail.com>
2016-08-31 00:32:13 -0300

    implementando trajetos

--- PBL3/pbl3/src/model/Auxiliar.java
+++ PBL3/pbl3/src/model/Auxiliar.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import util.Quadrante;
 
 /**
  *
@@ -49,6 +50,59 @@ public class Auxiliar {
         } catch (IOException ex) {
             System.out.println("não foi possivel estabelecer conexao");
 
+        }
+    }
+    
+    public ArrayList<Quadrante> calcularTrajeto(String origem, String destino){
+        ArrayList<Quadrante> trajeto = new ArrayList<Quadrante>();
+        if(origem.equals("A")){
+            trajeto.add(new Quadrante("d", 0, 0, 1));
+            if(destino.equals("B")){
+                return trajeto;
+            }else if(destino.equals("C")){
+                trajeto.add(new Quadrante("b", 0, 0, 2));
+                return trajeto;
+            }else{
+                trajeto.add(new Quadrante("b", 0, 0, 2));
+                trajeto.add(new Quadrante("a", 0, 0, 3));
+                return trajeto;
+            }
+        }else if(origem.equals("B")){
+            trajeto.add(new Quadrante("b", 0, 0, 1));
+            if(destino.equals("C")){
+                return trajeto;
+            }else if(destino.equals("D")){
+                trajeto.add(new Quadrante("a", 0, 0, 2));
+                return trajeto;
+            }else{
+                trajeto.add(new Quadrante("a", 0, 0, 2));
+                trajeto.add(new Quadrante("c", 0, 0, 3));
+                return trajeto;
+            }
+        }else if(origem.equals("C")){
+            trajeto.add(new Quadrante("a", 0, 0, 1));
+            if(destino.equals("D")){
+                return trajeto;
+            }else if(destino.equals("A")){
+                trajeto.add(new Quadrante("c", 0, 0, 2));
+                return trajeto;
+            }else{
+                trajeto.add(new Quadrante("c", 0, 0, 2));
+                trajeto.add(new Quadrante("d", 0, 0, 3));
+                return trajeto;
+            }
+        }else {
+            trajeto.add(new Quadrante("c", 0, 0, 1));
+            if(destino.equals("A")){
+                return trajeto;
+            }else if(destino.equals("B")){
+                trajeto.add(new Quadrante("d", 0, 0, 2));
+                return trajeto;
+            }else{
+                trajeto.add(new Quadrante("d", 0, 0, 2));
+                trajeto.add(new Quadrante("b", 0, 0, 3));
+                return trajeto;
+            }
         }
     }
 }
