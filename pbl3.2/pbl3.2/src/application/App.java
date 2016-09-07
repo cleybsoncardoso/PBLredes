4764f74bfe68e7ac987eaa038e2f9459fbe7f31b
lucaspaiva1 <lukspaiva95@gmail.com>
2016-09-07 02:06:32 -0300

    conexao

--- pbl3.2/pbl3.2/src/application/App.java
+++ pbl3.2/pbl3.2/src/application/App.java
@@ -33,7 +33,7 @@ public class App {
         
         Inicio telaInicial = new Inicio(controller);
         System.out.println("saiu");
-        //controller.iniciarConexao("25.12.22.120");
+        //controller.iniciarConexao("25.4.73.30");
         //controller.primeiraConexao("25.12.22.120");
         Server serverSocket = new Server(controller, 8080);
 
