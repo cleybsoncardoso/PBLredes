d115601eade24a9b4faeb48ddd688d06c3ab9414
Cleybson Cardoso <cleybsonc@gmail.com>
2016-09-28 01:21:43 -0300

    troca de mensagens

--- pbl3.1/src/util/TrataCliente.java
+++ pbl3.1/src/util/TrataCliente.java
@@ -10,7 +10,9 @@ import controller.ControllerCarro;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
@@ -28,17 +30,23 @@ import view.Inicio;
 public class TrataCliente implements Runnable {
 
     private Controller controller;
-     private String cor;
+    private String cor;
     private Quadrante quadranteAtual; //Quarda o quadrante do usuario
     private Verificacao verifica;
     private ArrayList<String> chavesips;
+    private String meuIp;
 
     public TrataCliente() {
         this.controller = Controller.getInstance();//pega instancia do controler atual
         this.quadranteAtual = new Quadrante("");
         chavesips = new ArrayList<String>();
         verifica = new Verificacao(this);
-        new Thread (verifica).start();
+        new Thread(verifica).start();
+        try {
+            meuIp = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(TrataCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
@@ -51,38 +59,39 @@ public class TrataCliente implements Runnable {
             //todas essas informações são recebidas através de um array
 
             String[] mensagem = Multicast.getInstancia().receberMensagem().split(";");
-            
-            float x = Float.parseFloat(mensagem[1]);
-            float y = Float.parseFloat(mensagem[2]);
-            int direcao = Integer.parseInt(mensagem[3]);
-            boolean parado = Boolean.parseBoolean(mensagem[4]);
-            //o trajeto é enviado quadrante por quadrante, e assim a junção é feita nessa instrução abaixo
-            int tamanhoDoTrajeto = Integer.parseInt(mensagem[5]);
-            for (int j = 6; j < tamanhoDoTrajeto + 6; j++) {
-                trajeto.add(new Quadrante(mensagem[j]));
-            }
-            
-            StringTokenizer tokenIp = new StringTokenizer(mensagem[0], ".");
-            tokenIp.nextToken();
-            tokenIp.nextToken();
-            String chaveHach = tokenIp.nextToken() + tokenIp.nextToken();
-            chavesips.add(chaveHach);
-            if(chavesips.contains(chaveHach)){
-                ControllerCarro carroAtual = controller.getCarro(chaveHach);
-                carroAtual.setXY(x, y, direcao);
-                carroAtual.setTrajeto(trajeto);
+            if (!meuIp.equals(mensagem[0])) {
+                float x = Float.parseFloat(mensagem[1]);
+                float y = Float.parseFloat(mensagem[2]);
+                int direcao = Integer.parseInt(mensagem[3]);
+                boolean parado = Boolean.parseBoolean(mensagem[4]);
+                //o trajeto é enviado quadrante por quadrante, e assim a junção é feita nessa instrução abaixo
+                int tamanhoDoTrajeto = Integer.parseInt(mensagem[5]);
+                for (int j = 6; j < tamanhoDoTrajeto + 6; j++) {
+                    trajeto.add(new Quadrante(mensagem[j]));
+                }
 
-                if (!quadranteAtual.getNome().equals(trajeto.get(0).getNome())) {//verifica se o carro ainda está no quadrante para pode exibir a msg
-                    Inicio.getInstance().mostrar("Carro " + cor + " saindo da pista " + quadranteAtual.getNome());
-                    Inicio.getInstance().mostrar("Carro " + cor + " entrando  na pista " + trajeto.get(0).getNome());
+                StringTokenizer tokenIp = new StringTokenizer(mensagem[0], ".");
+                tokenIp.nextToken();
+                tokenIp.nextToken();
+                String chaveHach = tokenIp.nextToken() + tokenIp.nextToken();
+                chavesips.add(chaveHach);
+                if (chavesips.contains(chaveHach)) {
+                    ControllerCarro carroAtual = controller.getCarro(chaveHach);
+                    carroAtual.setXY(x, y, direcao);
+                    carroAtual.setTrajeto(trajeto);
+
+                    if (!quadranteAtual.getNome().equals(trajeto.get(0).getNome())) {//verifica se o carro ainda está no quadrante para pode exibir a msg
+                        Inicio.getInstance().mostrar("Carro " + cor + " saindo da pista " + quadranteAtual.getNome());
+                        Inicio.getInstance().mostrar("Carro " + cor + " entrando  na pista " + trajeto.get(0).getNome());
+                        quadranteAtual = trajeto.get(0);
+                    }
+                    carroAtual.noCruzamento(parado);
+                } else {
+                    chavesips.add(chaveHach);
+                    controller.adicionarCarro(chaveHach, x, y, direcao, trajeto);
+                    Inicio.getInstance().mostrar("iniciando carro " + cor + " na pista " + trajeto.get(0).getNome());
                     quadranteAtual = trajeto.get(0);
                 }
-                carroAtual.noCruzamento(parado);
-            }else{
-                chavesips.add(chaveHach);
-                controller.adicionarCarro(chaveHach, x, y, direcao, trajeto);
-                Inicio.getInstance().mostrar("iniciando carro " + cor + " na pista " + trajeto.get(0).getNome());
-                quadranteAtual = trajeto.get(0);
             }
         }
     }
@@ -90,5 +99,5 @@ public class TrataCliente implements Runnable {
     public ArrayList<String> getChavesips() {
         return chavesips;
     }
-    
+
 }
