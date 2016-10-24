4b3482062e65c9ebe99844348bc4ab4f4977ab39
cleybson cardoso <cleybsonc@gmail.com>
2016-10-24 06:20:19 -0200

    iniciando ot

---
+++ Pbl5cley/DutoDocsServer_1/src/Model/Documento.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author cleyb
+ */
+public class Documento {
+
+    private String nome, conteudo;
+
+    public Documento(String nome, String conteudo) {
+        this.nome = nome;
+        this.conteudo = conteudo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+}
--- Pbl5cley/DutoDocsServer_1/src/dutodocsserver/DutoDocsServer.java
+++ Pbl5cley/DutoDocsServer_1/src/dutodocsserver/DutoDocsServer.java
@@ -17,7 +17,6 @@ public class DutoDocsServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(System.currentTimeMillis());
         Controller controller = new Controller("127.0.0.1");
     }
     
